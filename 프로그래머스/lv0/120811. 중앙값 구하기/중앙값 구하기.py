8b3bbe8c247e4c9b44627963d1daa19807b57809
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2023-08-02 18:13:38 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,8 @@
+def solution(array):
+    array.sort()
+    answer = len(array) // 2
+    if len(array) % 2 == 1:
+        return array[answer]
+    return ((array[answer - 1] + array[answer]) / 2) 
+
+
