2d1e5ac6741bf25bc8b8a664a5fe59798f641c17
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2023-08-02 18:13:24 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,15 @@
+def solution(array):
+    dict = {}
+    
+    for num in array :
+        if num not in dict :
+            dict[num] = 1
+        else :
+            dict[num] += 1
+        
+    result = sorted(dict.items(), key = lambda x: x[-1], reverse = True)
+    
+    if len(result) <= 1 :
+        return result[0][0]
+    return result[0][0] if result[0][-1] != result[1][-1] else -1
+    
