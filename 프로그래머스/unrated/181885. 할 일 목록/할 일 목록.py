95b322259a49ede321a18153b4805cec1a70202e
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2023-08-09 15:23:57 +0900

    [unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,6 @@
 def solution(todo_list, finished):
     answer = []
     for i in range(len(finished)) :
-        if int(finished[i]) == 0 :
+        if finished[i] == False :
             answer.append(todo_list[i])
     return answer
