ba2a9d145ea02a892bdf4090e84e329562861f5f
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-03-07 20:51:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1452 ms, Memory: 76972 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+num_list = []
+for i in range(T) :
+    a = int(input())
+    num_list.append(a)
+num_list.sort()
+for i in num_list :
+    print(i)
