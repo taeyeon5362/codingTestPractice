3b45b011cbdea1b9202af10acc4dc9e58e9b4472
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-03-07 19:14:29 +0900

    [Bronze I] Title: A/B - 2, Time: 52 ms, Memory: 31120 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,2 +1,8 @@
-a, b =  map(int, input().split())
-print(a / b)
+a, b = map(int, input().split())
+c = (str(a//b) + ".")
+a = (a % b) * 10
+
+for i in range(1000):
+    c += str(a//b)
+    a = (a % b)*10
+print(c)
