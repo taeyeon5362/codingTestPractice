b46ca8292adf0c6f0c97e5ca326566b6829eeb7b
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-03-09 16:20:18 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,12 @@
+T = int(input())
+student = []
+for _ in range(T) :
+    x, y =  map(int, input().split())
+    student.append((x, y))
+    
+for i in student :
+    rank = 1
+    for j in student :
+        if i[0] < j[0] and i[1] < j[1] :
+            rank += 1
+    print(rank, end = " ")
