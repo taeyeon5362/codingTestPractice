0217bf3c6dff847c2dc3de1b52529aa752cd0908
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-03-28 13:03:47 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    for i in d :
+        if budget >= i :
+            budget -= i
+            answer += 1
+        else :
+            break
+    return answer
