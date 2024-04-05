ebaa21906bdced9cd4b78042da3cc524d3546237
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-04-05 15:47:27 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n) :
+    print('@' * n + ' ' * n * 3 + '@' * n) 
+for _ in range(n) :
+    print('@' * n * 5)
+for _ in range(n) :
+    print('@' * n + ' ' * n * 3 + '@' * n)
+for _ in range(n) :
+    print('@' * n * 5)
+for _ in range(n) :
+    print('@' * n + ' ' * n * 3 + '@' * n)
