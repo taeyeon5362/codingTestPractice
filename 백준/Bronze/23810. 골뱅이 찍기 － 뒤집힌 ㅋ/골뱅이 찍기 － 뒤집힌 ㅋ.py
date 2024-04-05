c85e31bccdb4ff334c2780a7b39ad80b317500f6
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-04-05 15:40:52 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for _ in range(n) :
+    print('@' * n * 5)
+for _ in range(n) :
+    print('@' * n)
+for _ in range(n) :
+    print('@' * n * 5)
+for _ in range(n) :
+    print('@' * n)
+    print('@' * n)
