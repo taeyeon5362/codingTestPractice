b8ac0c44cfd21be1d6211ac5f1ce4966a6e92a80
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-06-22 15:05:31 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def onetwothree(n):
+    if n <= 3:
+        dp = [1, 2, 4]
+        return dp[n-1]
+    elif n > 3:
+        dp = [1, 2, 4] + [0]*(n-3)
+        for i in range(3, n):
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+        return dp[-1]
+
+for _ in range(n):
+    print(onetwothree(int(input())))
