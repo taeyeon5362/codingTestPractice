1b2429a65f56250c8cd014fc444bd091f974afae
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-06-23 00:10:34 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,13 @@
+n = [list(map(int, input().split())) for _ in range(9)]
+
+max_num = 0
+max_row, max_col = 0, 0
+for i in range(9):
+    for j in range(9):
+        if max_num <= n[i][j]:
+            max_row = i + 1
+            max_col = j + 1
+            max_num = n[i][j]
+
+print(max_num)
+print(max_row, max_col)
