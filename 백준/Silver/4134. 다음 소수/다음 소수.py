a4eabb419e68a2848a19292cf01821201b2c786d
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-07-31 14:11:07 +0900

    [Silver IV] Title: 다음 소수, Time: 1472 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+def is_prime_number(x):
+    for i in range(2,int(x**0.5)+1):
+        if x % i==0:
+            return False
+    return True
+
+for _ in range(T) :
+    n = int(input())
+    while True :
+        if n==0 or n==1:
+            print(2)
+            break
+        if is_prime_number(n) :
+            print(n)
+            break
+        else :
+            n += 1
