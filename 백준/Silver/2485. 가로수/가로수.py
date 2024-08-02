ad5da6e23e3fce57ba077457f7db332dce0490c7
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-08-02 12:05:10 +0900

    [Silver IV] Title: 가로수, Time: 124 ms, Memory: 37096 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+from math import gcd
+
+N = int(input())
+a = int(input())
+
+arr = []
+
+for i in range(N-1):
+    num = int(input())
+    arr.append(num - a)
+    a = num
+
+g = arr[0]
+for j in range(1, len(arr)):
+    g = gcd(g, arr[j])
+
+result = 0
+for each in arr:
+    result += each // g - 1
+print(result)
