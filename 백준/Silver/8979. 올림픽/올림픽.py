cf2b3e45f7c8ef36e30ec470059c39748b55ee5d
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-09-06 16:13:21 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+medal = []
+
+for _ in range(n) :
+    k_, gold, silver, bronze = map(int, input().split())
+    medal.append((k_, gold, silver, bronze))
+
+medal.sort(key = lambda x : (x[1], x[2], x[3]), reverse=True)
+
+idx = [medal[i][0] for i in range(n)].index(k)
+
+for i in range(n):
+    if medal[idx][1:] == medal[i][1:]:
+        print(i+1)
+        break
