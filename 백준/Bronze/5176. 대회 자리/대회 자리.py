08fa11a395f70272116b4c6b235e1e65c3749757
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-09-23 19:49:07 -1200

    [Bronze II] Title: 대회 자리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5176. 대회 자리/대회 자리.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+
+for i in range(x):
+    wants = []
+    count = 0
+    people, seat = map(int, input().split())
+    for i in range(people):
+        wants.append(int(input()))
+    if seat == 1:
+        print(people - 1)
+    else:
+        count = 0
+        for i in range(1, seat+1):
+            if wants.count(i) >= 2:
+                count += wants.count(i) - 1
+        print(count)
